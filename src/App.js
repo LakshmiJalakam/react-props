9ab71c5ec3afd6e7b8a55c8b94a11816c604628f
LakshmiJalakam <jalakamlakshmiprasanna@gmail.com>
2022-11-30 16:51:23 +0530

    props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ //import ClassComponent from './ClassComponent'
+ import PropsExample from './PropsExample'
+ function App(){
+  return(
+    <section>
+    <PropsExample name="ganesh" age="19"/>
+    <PropsExample name="lucky" age="19"/>
+    {/*<p>Good Afternoon</p>
+    <h1>Heading Tag</h1>
+    <ClassComponent/>
+    <img src="https://img.freepik.com/free-photo/breathtaking-shot-beautiful-stones-turquoise-water-lake-hills-background_181624-12847.jpg?w=2000" alt="nature" width="25%"/>*/}
+    </section>
+    )
 }
-
-export default App;
+export default App
